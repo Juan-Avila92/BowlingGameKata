a1e94ad18617a06a5a65b2de3570bc9618988633
Juan-Avila92 <jc.avila1092@gmail.com>
2022-06-07 09:59:53 -0500

    Unit tests for Game class

---
+++ BowlingGameTests/GameTest.cs
@@ -0,0 +1,22 @@
+using BowlingGame;
+using NUnit.Framework;
+
+namespace BowlingGameTests
+{
+    public class GameTest
+    {
+        Game bowlingGame;
+
+        [SetUp]
+        public void Setup()
+        {
+            bowlingGame = new Game();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
